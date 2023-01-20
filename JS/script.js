de3b6c58407182426b78dacbcaf85d90900c442b
wacherawit <darthwww@gmail.com>
2023-01-21 00:56:02 +0700

    ainmate & responsive

--- JS/script.js
+++ JS/script.js
@@ -7,6 +7,8 @@ new Vivus('Mysvg', {
     function (obj) {
         obj.el.classList.add("finished")
         document.getElementById('Loading').classList.add("Fadeout");
+        document.getElementById('Under-nav').classList.add("Open");
+        document.getElementById('ME').classList.add("AB-open");
         setTimeout(function(){
             document.getElementById('Loading').style.display = 'none';
           }, 3000);
