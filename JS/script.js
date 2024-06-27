db9af248a4f0b2a9091ab082ebdc68f7f23f661b
wacherawit <darthwww@gmail.com>
2024-06-28 00:44:12 +0700

    Update

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,22 @@
+async function loadPage(page) {
+    const contentDiv = document.getElementById('content');
+    contentDiv.classList.remove('visible');
+
+    try {
+        const response = await fetch(page);
+        const html = await response.text();
+        contentDiv.innerHTML = html;
+        contentDiv.classList.add('visible');
+
+        // เรียกใช้สคริปต์ใหม่หลังจากที่เนื้อหาใหม่ถูกโหลด
+        const scripts = contentDiv.getElementsByTagName('script');
+        for (let i = 0; i < scripts.length; i++) {
+            eval(scripts[i].innerHTML);
+        }
+    } catch (error) {
+        console.error('Error loading page:', error);
+    }
+};
 new Vivus('Mysvg', {
     type: "oneByOne",
     delay: 0,
@@ -9,8 +28,8 @@ new Vivus('Mysvg', {
         document.getElementById('Loading').classList.add("Fadeout");
         document.getElementById('Under-nav').classList.add("Open");
         document.getElementById('ME').classList.add("AB-open");
-        setTimeout(function(){
+        setTimeout(function () {
             document.getElementById('Loading').style.display = 'none';
-          }, 3000);
+        }, 3000);
     }
 );
